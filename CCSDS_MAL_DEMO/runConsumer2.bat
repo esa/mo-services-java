e0dfe631bac3a1bae658dc3abf1c23ba42d410f4
Sam Cooper <sam.cooper@scisys.co.uk>
2014-10-28 16:34:08 +0000

    Updated scripts to reference correct JAR file.

--- CCSDS_MAL_DEMO/runConsumer2.bat
+++ CCSDS_MAL_DEMO/runConsumer2.bat
@@ -1,1 +1,1 @@
-java -classpath target\CCSDS_MAL_DEMO-1.0-SNAPSHOT-jar-with-dependencies.jar -Dapplication.name=Consumer2 esa.mo.mal.demo.consumer.DemoConsumerGui
+java -classpath target\MAL_DEMO-1.0-jar-with-dependencies.jar -Dapplication.name=Consumer2 esa.mo.mal.demo.consumer.DemoConsumerGui
--- CCSDS_MAL_DEMO/runProvider.bat
+++ CCSDS_MAL_DEMO/runProvider.bat
@@ -1,1 +1,1 @@
-java -classpath target\CCSDS_MAL_DEMO-1.0-SNAPSHOT-jar-with-dependencies.jar esa.mo.mal.demo.provider.DemoProviderGui
+java -classpath target\MAL_DEMO-1.0-jar-with-dependencies.jar esa.mo.mal.demo.provider.DemoProviderGui
