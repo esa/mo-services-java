7d403cfca1a01e55a2d65ca75030bc43546187e8
Sam Cooper <sam.cooper@scisys.co.uk>
2015-01-09 16:04:25 +0000

    Updated to version 1.0.1 for existing demo. Initial version 1.0 for MAL and COM support libs. Released to Maven.

--- CCSDS_MAL_DEMO/runProviderCli-linux.sh
+++ CCSDS_MAL_DEMO/runProviderCli-linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ${0%/*}
-java -classpath "./target/MAL_DEMO-1.0-jar-with-dependencies.jar" esa.mo.mal.demo.provider.DemoProviderCli
+java -classpath "./target/MAL_DEMO-1.0.1-jar-with-dependencies.jar" esa.mo.mal.demo.provider.DemoProviderCli
