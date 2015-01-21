b42c37c849846d2e52b0f20daade2f419cc2f06e
Stefan Gärtner <stefan.gaertner@dlr.de>
2015-01-21 11:12:29 +0100

    Prepared for version 1.0 release of DLR transport and encoding implementation.
    - Updated dependency versions for DLR implementation.
    - Updated licence headers.

--- MOIMS_TESTBED_MALSPP/src/test/java/org/ccsds/moims/mo/malspp/test/suite/JUnitSuiteTest.java
+++ MOIMS_TESTBED_MALSPP/src/test/java/org/ccsds/moims/mo/malspp/test/suite/JUnitSuiteTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.ccsds.moims.mo.malspp.test.suite;
 
 import fitnesse.junit.FitNesseRunner;
