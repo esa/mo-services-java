12699fd35bf2656e8ce015610a71b45087741d16
CesarCoelho <cbwhittle@hotmail.com>
2023-11-30 10:35:20 +0100

    Separates the Outgoing Channels out of the Transport class into a dedicated class

--- bindings/transport-generic/src/main/java/esa/mo/mal/transport/gen/Transport.java
+++ bindings/transport-generic/src/main/java/esa/mo/mal/transport/gen/Transport.java
@@ -67,23 +67,10 @@ public abstract class Transport<I, O> implements MALTransport {
     public static final String DEBUG_PROPERTY
             = "org.ccsds.moims.mo.mal.transport.gen.debug";
     /**
-     * System property to control the number of connections per client.
-     */
-    public static final String NUM_CLIENT_CONNS_PROPERTY
-            = "org.ccsds.moims.mo.mal.transport.gen.numconnections";
-    /**
      * The timeout in seconds to wait for confirmation of delivery.
      */
     public static final String DELIVERY_TIMEOUT_PROPERTY
             = "org.ccsds.moims.mo.mal.transport.gen.deliverytimeout";
-
-    /**
-     * System property to control whether a new outgoing data channel should be
-     * created if one dosen't exist when sending a message
-     */
-    public static final String CONNECT_WHEN_CONSUMER_OFFLINE_PROPERTY
-            = "org.ccsds.moims.mo.mal.transport.gen.connectwhenconsumeroffline";
-
     /**
      * Charset used for converting the encoded message into a string for
      * debugging.
@@ -156,12 +143,6 @@ public abstract class Transport<I, O> implements MALTransport {
      */
     protected final Map qosProperties;
     /**
-     * The number of connections per client or server. The Transport will
-     * connect numConnections times to the predefined port and host per
-     * different client/server.
-     */
-    private final int numConnections;
-    /**
      * The thread that receives incoming message from the underlying transport.
      * All incoming raw data packets are processed by this thread.
      */
@@ -180,7 +161,7 @@ public abstract class Transport<I, O> implements MALTransport {
      * Map of outgoing channels. This associates a URI to a transport resource
      * that is able to send messages to this URI.
      */
-    private final Map<String, ConcurrentMessageSender> outgoingDataChannels = new HashMap<>();
+    private final OutgoingDataChannels outgoingDataChannelsManager;
     /**
      * The stream factory used for encoding and decoding messages.
      */
@@ -193,18 +174,6 @@ public abstract class Transport<I, O> implements MALTransport {
      * Map of cachedRoutingParts. This associates a URI to its Routing part.
      */
     private final ConcurrentHashMap<String, String> cachedRoutingParts = new ConcurrentHashMap<>();
-    /**
-     * Value of the
-     * org.ccsds.moims.mo.mal.transport.gen.connectwhenconsumeroffline property
-     */
-    private boolean connectWhenConsumerOffline = true;
-
-    /**
-     * Set of root uris to which the transport tried to connect. Used together
-     * with the connectWhenConsumerOffline property to decide if the connection
-     * is trying to be established for the first time.
-     */
-    private final static Set<String> connectionAttempts = new HashSet<>();
 
     /**
      * Constructor.
@@ -274,7 +243,6 @@ public abstract class Transport<I, O> implements MALTransport {
         // default values
         boolean lWrapBodyParts = wrapBodyParts;
         boolean lInProcessSupport = true;
-        int lNumConnections = 1;
         int lDeliveryTime = 10;
 
         // decode configuration
@@ -287,24 +255,15 @@ public abstract class Transport<I, O> implements MALTransport {
                 lInProcessSupport = Boolean.parseBoolean((String) properties.get(INPROC_PROPERTY));
             }
 
-            // number of connections per client/server
-            if (properties.containsKey(NUM_CLIENT_CONNS_PROPERTY)) {
-                lNumConnections = Integer.parseInt((String) properties.get(NUM_CLIENT_CONNS_PROPERTY));
-            }
-
             if (properties.containsKey(DELIVERY_TIMEOUT_PROPERTY)) {
                 lDeliveryTime = Integer.parseInt((String) properties.get(DELIVERY_TIMEOUT_PROPERTY));
-            }
-
-            if (properties.containsKey(CONNECT_WHEN_CONSUMER_OFFLINE_PROPERTY)) {
-                connectWhenConsumerOffline = Boolean.parseBoolean((String) properties.get(CONNECT_WHEN_CONSUMER_OFFLINE_PROPERTY));
             }
         }
 
         this.wrapBodyParts = lWrapBodyParts;
         this.inProcessSupport = lInProcessSupport;
-        this.numConnections = lNumConnections;
         this.deliveryTimeout = lDeliveryTime;
+        this.outgoingDataChannelsManager = new OutgoingDataChannels(this, properties);
 
         TransportThreadFactory decFactory = new TransportThreadFactory("Transport_Decoder");
         this.decoderExecutor = Executors.newSingleThreadExecutor(decFactory);
@@ -475,24 +434,7 @@ public abstract class Transport<I, O> implements MALTransport {
         }
 
         if (localUriTo != null) {
-            ConcurrentMessageSender commsChannel;
-
-            synchronized (this) {
-                commsChannel = outgoingDataChannels.get(localUriTo);
-                if (commsChannel != null) {
-                    outgoingDataChannels.remove(localUriTo);
-                } else {
-                    LOGGER.log(Level.WARNING,
-                            "Could not locate associated data channel to close "
-                            + "communications, perhaps it was closed before. URI : {0} ",
-                            localUriTo);
-                }
-            }
-            if (commsChannel != null) {
-                // need to do this outside the sync block so 
-                // that we do not affect other threads
-                commsChannel.terminate();
-            }
+            outgoingDataChannelsManager.closeConnection(localUriTo);
         }
 
         if (receptionHandler != null) {
@@ -537,15 +479,9 @@ public abstract class Transport<I, O> implements MALTransport {
         decoderExecutor.shutdown();
         dispatcherExecutor.shutdown();
 
-        LOGGER.fine("Closing outgoing channels");
         synchronized (this) {
-            for (ConcurrentMessageSender sender : outgoingDataChannels.values()) {
-                sender.terminate();
-            }
-
-            outgoingDataChannels.clear();
+            outgoingDataChannelsManager.closeAll();
         }
-        LOGGER.fine("Closed outgoing channels");
     }
 
     /**
@@ -790,54 +726,14 @@ public abstract class Transport<I, O> implements MALTransport {
                 receptionHandler.setRemoteURI(sourceRootURI);
 
                 //register the communication channel with this URI if needed
-                sender = registerMessageSender(receptionHandler.getMessageSender(), sourceRootURI);
+                sender = outgoingDataChannelsManager.registerMessageSender(receptionHandler.getMessageSender(), sourceRootURI);
             }
         } else {
             // outgoing message
             // get target URI
-            URI reroutedMsg = rerouteMessage(msg);
+            URI reroutedMsg = this.rerouteMessage(msg);
             String remoteRootURI = reroutedMsg.getRootURI(serviceDelim, serviceDelimCounter);
-
-            // get sender if it exists
-            sender = outgoingDataChannels.get(remoteRootURI);
-
-            boolean firstTime = !connectionAttempts.contains(remoteRootURI);
-            if (firstTime) {
-                connectionAttempts.add(remoteRootURI);
-            }
-
-            if (sender == null && (connectWhenConsumerOffline || firstTime)) {
-                // we do not have any channel for this URI
-                // try to create a set of connections to this URI 
-                LOGGER.log(Level.INFO, "Establishing connection to: {0}", remoteRootURI);
-
-                try {
-                    // create new sender for this URI
-                    MessageSender transmitter = createMessageSender(msg.getHeader(), remoteRootURI);
-                    sender = registerMessageSender(transmitter, remoteRootURI);
-
-                    LOGGER.log(Level.FINE, "Opening {0}", numConnections);
-
-                    for (int i = 1; i < numConnections; i++) {
-                        // insert new processor (message sender) to root data sender for the URI
-                        MessageSender anotherTransmitter = createMessageSender(msg.getHeader(), remoteRootURI);
-                        sender.addProcessor(anotherTransmitter, remoteRootURI);
-                    }
-                } catch (MALException e) {
-                    LOGGER.log(Level.WARNING,
-                            "Could not connect to: " + remoteRootURI, e);
-
-                    throw new MALTransmitErrorException(msg.getHeader(),
-                            new DestinationUnknownException(null),
-                            null);
-                }
-            } else if (sender == null && !connectWhenConsumerOffline) {
-                LOGGER.log(Level.FINE, "Could not locate an outgoing data channel and "
-                        + "the connectWhenConsumerOffline property prevents establishing a new one");
-                throw new MALTransmitErrorException(msg.getHeader(),
-                        new DestinationUnknownException(null),
-                        null);
-            }
+            sender = outgoingDataChannelsManager.manageCommunicationChannelOutgoing(msg, remoteRootURI);
         }
 
         return sender;
@@ -845,48 +741,6 @@ public abstract class Transport<I, O> implements MALTransport {
 
     protected URI rerouteMessage(GENMessage message) {
         return message.getHeader().getToURI();
-    }
-
-    /**
-     * Registers a message sender for a given root URI. If this is the first
-     * data sender for the URI, it also creates a GENConcurrentMessageSender to
-     * manage all the senders. If there are already enough connections
-     * (numConnections) to the given URI the method does not register the
-     * sender. This ensures that we will have at maximum numConnections to the
-     * target root URI.
-     *
-     * @param dataTransmitter The data sender that is able to send messages to
-     * the URI
-     * @param remoteRootURI the remote root URI
-     * @return returns the GENConcurrentMessageSender for this URI.
-     */
-    protected synchronized ConcurrentMessageSender registerMessageSender(
-            MessageSender dataTransmitter, String remoteRootURI) {
-        //check if we already have a communication channel for this URI
-        ConcurrentMessageSender dataSender = outgoingDataChannels.get(remoteRootURI);
-
-        if (dataSender != null) {
-            //we already have a communication channel for this URI
-            //check if we have enough connections for the URI, if not then add the data sender 
-            if (dataSender.getNumberOfProcessors() < numConnections) {
-                LOGGER.log(Level.FINE, "Registering data sender for URI: {0}", remoteRootURI);
-                // insert new processor (message sender) to root data sender for the URI
-                dataSender.addProcessor(dataTransmitter, remoteRootURI);
-            }
-        } else {
-            //we do not have a communication channel, create a data sender manager and add the first data sender
-            // create new sender manager for this URI
-            LOGGER.log(Level.FINE, "Creating data sender manager for URI: {0}", remoteRootURI);
-            dataSender = new ConcurrentMessageSender(this, remoteRootURI);
-
-            LOGGER.log(Level.FINE, "Registering data sender for URI: {0}", remoteRootURI);
-            outgoingDataChannels.put(remoteRootURI, dataSender);
-
-            // insert new processor (message sender) to root data sender for the URI
-            dataSender.addProcessor(dataTransmitter, remoteRootURI);
-        }
-
-        return dataSender;
     }
 
     /**
