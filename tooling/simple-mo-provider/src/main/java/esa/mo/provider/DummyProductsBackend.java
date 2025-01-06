808a851d270f5cfb93afc4267c0b2e1455bde1ab
CesarCoelho <cbwhittle@hotmail.com>
2025-01-06 10:19:55 +0100

    Updates the Backend to use the Dataset class

--- tooling/simple-mo-provider/src/main/java/esa/mo/provider/DummyProductsBackend.java
+++ tooling/simple-mo-provider/src/main/java/esa/mo/provider/DummyProductsBackend.java
@@ -20,7 +20,6 @@
  */
 package esa.mo.provider;
 
-import java.util.HashMap;
 import org.ccsds.moims.mo.mal.structures.Blob;
 import org.ccsds.moims.mo.mal.structures.Identifier;
 import org.ccsds.moims.mo.mal.structures.IdentifierList;
@@ -28,22 +27,16 @@ import org.ccsds.moims.mo.mal.structures.ObjectIdentity;
 import org.ccsds.moims.mo.mal.structures.ObjectRef;
 import org.ccsds.moims.mo.mal.structures.Time;
 import org.ccsds.moims.mo.mal.structures.UInteger;
-import org.ccsds.moims.mo.mpd.backends.ProductRetrievalBackend;
+import org.ccsds.moims.mo.mpd.Dataset;
 import org.ccsds.moims.mo.mpd.structures.Product;
 import org.ccsds.moims.mo.mpd.structures.ProductMetadata;
-import org.ccsds.moims.mo.mpd.structures.ProductMetadataList;
 import org.ccsds.moims.mo.mpd.structures.ProductType;
 import org.ccsds.moims.mo.mpd.structures.TimeWindow;
 
 /**
  * A dummy backend in order to try out the provider.
  */
-public class DummyProductsBackend implements ProductRetrievalBackend {
-
-    private final HashMap<ObjectRef, ProductType> productTypes = new HashMap();
-    private final HashMap<ObjectRef, Product> products = new HashMap();
-    private final HashMap<ObjectRef, ProductMetadata> metadatas = new HashMap();
-    private final ProductMetadataList allMetadatas = new ProductMetadataList();
+public class DummyProductsBackend extends Dataset {
 
     public DummyProductsBackend() {
         IdentifierList domain = new IdentifierList();
@@ -60,28 +53,11 @@ public class DummyProductsBackend implements ProductRetrievalBackend {
         // ---------------------------------------------------
         TimeWindow timeWindow = new TimeWindow(Time.now(), Time.now());
         ObjectIdentity productId = new ObjectIdentity(domain, new Identifier("key1"), new UInteger(1));
-        Product product = new Product(productId, type1, Time.now(), timeWindow, new Blob());
+        Blob productBody = new Blob();
+        Product product = new Product(productId, type1, Time.now(), timeWindow, productBody);
         ObjectRef<Product> ref = product.getObjectRef();
         ProductMetadata metadata = new ProductMetadata();
-
-        products.put(ref, product);
-        metadatas.put(ref, metadata);
-        allMetadatas.add(metadata);
-    }
-
-    @Override
-    public ProductMetadataList getMetadataFromAllProducts() {
-        return allMetadatas;
-    }
-
-    @Override
-    public Product getProduct(ObjectRef productRef) {
-        return products.get(productRef);
-    }
-
-    @Override
-    public ProductMetadata getMetadata(ObjectRef productRef) {
-        return metadatas.get(productRef);
+        super.addNewProduct(ref, productBody, metadata);
     }
 
 }
