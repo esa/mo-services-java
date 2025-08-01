7a055af9c119af2845f6a8e668cae5ed8c3d09cc
Cesar Coelho <cbwhittle@hotmail.com>
2025-08-01 12:04:28 +0200

    Small textual updates

--- testbeds/testbed-mc/src/main/java/org/ccsds/mo/mc/testbed/backends/package-info.java
+++ testbeds/testbed-mc/src/main/java/org/ccsds/mo/mc/testbed/backends/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Package containing the MPD testbed backends. These will be used by the testing classes.
+ * Package containing the MC testbed backends. These will be used by the testing classes.
  */
 package org.ccsds.mo.mc.testbed.backends;
